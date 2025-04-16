f23537bef1bb920c3a30369a3c81a3e4c9de5fe3
Josiah <josiahbschlabach@gmail.com>
2025-04-16 16:25:08 -0400

    getdata function and start of Re Num Functions

--- scripts/DataAnalysis_Obj1.py
+++ scripts/DataAnalysis_Obj1.py
@@ -3,7 +3,23 @@ import pandas as pd
 import numpy as np
 
 
-def data():
+def getdata():
+    """
+    Using the CSV obtained from the UOLab server, this functions grabs the
+    data needed to complete this script. Specifically, this grabs the control
+    valves position (% open) and flow meter reading.
+
+    Parameters
+    ----------
+    None.
+
+    Returns
+    ----------
+    FIC : array
+        The values of the control valve % open
+    FIT : array
+        The value of each flow meter in GPM
+    """
     df = pd.read_csv("AREA400-4-9-2025_Test1.csv")
     data = df.to_numpy()
 
@@ -12,27 +28,34 @@ def data():
     # Columns 2 & 3 are valve opening position for FIC-400B (note decrepsencies in data) (in order BLOCK1 PID1)
     # Columns 6 & 7 are valve opening position for FIC-400C (in order BLOCK1 PID1)
 
-    # Make these two arrays if possible, for simplicity
-    FIC_B = data[:, 3]
-    FIC_C = data[:, 7]
-    FIT_A = data[:, 11]
-    FIT_B = data[:, 12]
-    FIT_C = data[:, 13]
-    FIT_D = data[:, 14]
+    # FIC grabs from PID1
+    FIC = data[:, [3, 7]]  # In order: B, C
+    FIT = data[:, [11, 12, 13, 14]]  # In order: A, B, C, D
 
-    FIT = data[:, [11, 14]]
-    print(FIT_A)
-    print()
-    print(FIT_B)
-    print()
-    print(FIT_C)
-    print()
-    print(FIT_D)
-    print()
-    print(FIT)
+    return FIC, FIT
 
-    return
+
+def ReNum(Q, D, nu):
+    """
+    Obtaines the Reynolds Number using the volumetric flow rate
+
+    Parameters
+    ----------
+    Q : array
+        Volumetric flow rate as provided by FIT-400B
+    D : int
+        Diameter of Vortex Flow meter
+    nu : int
+        Kinematic Viscoity of water at given temp
+
+    Returns
+    ----------
+    Re : array
+        Reynold's number with respect to FIT-400C
+
+    """
+    return Q * D / nu
 
 
 if __name__ == "__main__":
-    data()
+    FIC, FIT = getdata()
