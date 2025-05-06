a100f634be85ac0792273bbfcb3e96e4a39215fe
Josiah <josiahbschlabach@gmail.com>
2025-05-06 14:05:14 -0400

    Characteristic Curve using Averages

--- scripts/DataAnalysis_Obj2.py
+++ scripts/DataAnalysis_Obj2.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use("ggplot")
+
 
 def main():
     FIC, FIT = getdata("../data/AREA400-2025-04-30_FIC-400C_Obj1_AllRep.csv")
     # vec = flow_percent(FIT)
     # print(vec)
     makeplot(FIC, FIT)
-    # avg_flow(FIC, FIT)
+    # avg = avg_flowrates(FIC, FIT)
+    # flow_percent(avg)
 
 
 def getdata(path):
@@ -45,14 +48,22 @@ def getdata(path):
     return FIC, FIT
 
 
-def flow_percent(FIT):
-    max = np.max(FIT)
+# def flow_percent(FIT):
+#     max = np.max(FIT)
+#
+#     p_flow = (FIT / max) * 100
+#     return p_flow
 
-    p_flow = (FIT / max) * 100
+
+def flow_percent(avg_flow):
+    max = np.max(avg_flow[1])
+
+    p_flow = (avg_flow[1] / max) * 100
+    print(p_flow)
     return p_flow
 
 
-def avg_flow(FIC, FIT):
+def avg_flowrates(FIC, FIT):
 
     FIT = FIT.reshape(-1, 1)
     FIT = FIT[:, 0]
@@ -67,14 +78,18 @@ def avg_flow(FIC, FIT):
     data_sort = np.take_along_axis(data, index, axis=0)
     data = data_sort.transpose()
 
-    # for i in range(len(data[0])):
-    #     check = data[0, i]
-    #     if data[0, i] == check:
-    #
+    valve_opening = data[0]
+    flow_rates = data[1]
 
-    # for i in range(len(data[:,0])):
+    unique_openings, inverse_indicies = np.unique(
+        valve_opening, return_inverse=True
+    )
+    avg_flow = np.zeros_like(unique_openings, dtype=float)
 
-    return
+    for i in range(len(unique_openings)):
+        avg_flow[i] = np.mean(flow_rates[inverse_indicies == i])
+
+    return np.vstack((unique_openings, avg_flow))
 
 
 def makeplot(FIC, FIT):
@@ -82,18 +97,11 @@ def makeplot(FIC, FIT):
     Using the data from the passed in csv, plot a chart of the of the flow percent
     against the stem opening for each valve
     """
-    p_flow = flow_percent(FIT)
-    FICB = FIC[0]
-    FICC = FIC[1]
     fig, ax1 = plt.subplots()
+    avg_flow = avg_flowrates(FIC, FIT)
+    p_flow = flow_percent(avg_flow)
 
-    if (len(set(FICB)) == 1) is False:
-        ax1.scatter(FICB, p_flow)
-        print("FICB")
-    elif (len(set(FICC)) == 1) is False:
-        ax1.scatter(FICC, p_flow)
-        print("FICC")
-
+    ax1.scatter(avg_flow[0], p_flow)
     ax1.set_xlabel("Stem Opening (%)")
     ax1.set_ylabel("Flow %")
     ax1.set_xlim(-5, 110)
